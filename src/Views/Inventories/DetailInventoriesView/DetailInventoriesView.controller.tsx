830b1c12379918fc973a2adfc98bc98cbc4831cf
Raul Salinas <raul.vam.egas@comunidad.unam.mx>
2025-03-25 00:07:12 -0600

    Cleaned code and fixed translations

--- src/Views/Inventories/DetailInventoriesView/DetailInventoriesView.controller.tsx
+++ src/Views/Inventories/DetailInventoriesView/DetailInventoriesView.controller.tsx
@@ -8,7 +8,7 @@ import { useIsFocused } from '@react-navigation/native';
 // External Dependencies
 
 // Internal Dependencies
-
+import { Unit } from '../../../viewModels/useUnits/useUnits.model';
 import useRoute from "../../../navigation/useRoute/useRoute";
 import { useAppSelector } from "../../../store/hooks";
 import { useMainContext } from "../../../contexts/mainContext";
@@ -30,11 +30,67 @@ const useDetailNotes = () => {
   const route = useRoute({ screenName: 'DetailInventoriesView' });
   const { inventory } = route.params || {};
   // Extract the view models from the context
+  const language = useAppSelector((state) => state.config.language)
+  const date = formatLongDate(inventory.date_created, language)
+
+    
+  const getTranslatedUnit = (item: Unit) => {
+    return language === "EN" ? item.nameEng : item.nameSpa;
+  }
+
+  const firstName = inventory.user_created.first_name || "";
+  const lastName = inventory.user_created.last_name || "";
+
+  const issuedBy = firstName + " " + lastName;
+
+  const shrinkageValues = inventory.products.reduce((acc, product) => {
+    const productInfo = product.idFabricatedProducts || product.idRawProducts;
+    
+    if (!productInfo) return acc;
+    
+    const difference = Number(product.countedUnits) - Number(product.expectedUnits);
+    const retailPrice = productInfo.retailPrice || 0;
+    const wholesalePrice = productInfo.wholesalePrice || 0;
+    
+    const retailShrinkage = difference * retailPrice;
+    const wholesaleShrinkage = difference * wholesalePrice;
+    
+    return {
+      shrinkageRetail: acc.shrinkageRetail + retailShrinkage,
+      shrinkageWholesale: acc.shrinkageWholesale + wholesaleShrinkage,
+      totalExpectedRetail: acc.totalExpectedRetail + (Number(product.expectedUnits) * retailPrice),
+      totalExpectedWholesale: acc.totalExpectedWholesale + (Number(product.expectedUnits) * wholesalePrice)
+    };
+  }, {
+    shrinkageRetail: 0,
+    shrinkageWholesale: 0,
+    totalExpectedRetail: 0,
+    totalExpectedWholesale: 0
+  });
   
+  // Calcula porcentajes
+  const shrinkageRetailPercentage = (shrinkageValues.shrinkageRetail / shrinkageValues.totalExpectedRetail) * 100 || 0;
+  
+  const shrinkageWholesalePercentage = (shrinkageValues.shrinkageWholesale / shrinkageValues.totalExpectedWholesale) * 100 || 0;
+
+  const segments = [
+    'CATA_SEGMENT_RAW',
+    'CATA_SEGMENT_FAB',
+  ]
+  const [segmentSelected, setSegmentSelected] = useState(segments[0]);
 
 
   return {
-    inventory
+    inventory,
+    date,
+    issuedBy,
+    shrinkageValues,
+    shrinkageRetailPercentage,
+    shrinkageWholesalePercentage,
+    segments,
+    segmentSelected,
+    setSegmentSelected,
+    getTranslatedUnit
   }
 }
 
